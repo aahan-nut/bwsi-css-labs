304f3476783fbd4cb2c996a661602426d99d4b1a
aahan-nut <aahannutella@gmail.com>
2026-03-15 11:36:47 -0700

    sanitize fix

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -37,13 +37,20 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
 
+def sanitize_num(text: str) -> float:
+    while True: 
+        try:
+            num = float(input(text))
+            return num
+        except ValueError:
+            print("Not valid input, enter a number")
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = sanitize_num("Enter the first number: ")
+    num2 = sanitize_num("Enter the second number: ")
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
